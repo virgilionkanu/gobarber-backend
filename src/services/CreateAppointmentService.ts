031980362d6b0b7e633f30345a19e4aed5aa06ef
Virgilio Cano <virgiliocano53@gmail.com>
2020-11-09 18:54:37 -0300

    Saving information on database

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/appointment';
 import AppointmentsRepository from '../repositories/appointmentsRepository';
@@ -9,16 +10,14 @@ interface Request {
 }
 
 class CreateAppointMentService {
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ provider, date }: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository,
+        );
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
-
-    public execute({ provider, date }: Request): Appointment {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
             appointmentDate,
         );
 
@@ -26,10 +25,12 @@ class CreateAppointMentService {
             throw Error('This appointment is already booked.');
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate,
         });
+
+        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
